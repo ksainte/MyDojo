8ed1927407adf42aaa56618f82620ad1b1fdfe36
ks <ksainte19@gmail.com>
2024-09-01 01:13:14 +0200

    ok

--- src/app/Cotisation_Page.js
+++ src/app/Cotisation_Page.js
@@ -12,17 +12,17 @@ const CotisationPage = () => {
                 </div>
             </div>
 
-            <div className="flex justify-center w-full mt-10">
-                <div className="flex justify-evenly items-center w-full max-w-screen-2xl border background p-3">
+            <div className="flex justify-center w-full mt-10" >
+                <div className="flex max-md:flex-col max-md:gap-4 justify-evenly items-center w-full  border background p-3 xxxs:max-w-screen-xxxs xxs:max-w-screen-xxs xs:max-w-screen-xxs md:max-w-screen-lg lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
                     <div className="flex flex-col">
                         <div className="bg-white p-3">
-                            <div className="border border-black flex flex-col gap-2 p-16 bg-white items-center">
+                            <div className="border border-black flex flex-col gap-2 p-16 max-lg:p-8 bg-white items-center">
                                 <div className="flex gap-2 items-center">
-                                    <div className="lg:text-lg xl:text-xl 2xl:text-2xl">tarif</div>
-                                    <div className="underline font-bold lg:text-xl xl:text-2xl 2xl:text-3xl">ADULTE</div>
+                                    <div className="xxxs:text-md xxs:text-lg lg:text-lg xl:text-xl 2xl:text-2xl">tarif</div>
+                                    <div className="underline font-bold xxxs:text-xl xxs:text-2xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono">ADULTE</div>
                                 </div>
-                                <div className="lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-green-500">65€</div>
-                                <div className="lg:text-lg xl:text-xl 2xl:text-2xl">/mois</div>
+                                <div className="xxxs:text-2xl xxs:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-green-500 ">65€</div>
+                                <div className="xxxs:text-lg xxs:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">/mois</div>
                             </div>
                         </div>
                         <div className="bg-black flex flex-col gap-2 p-10 text-white items-start">
@@ -33,13 +33,13 @@ const CotisationPage = () => {
                     </div>
                     <div className="flex flex-col">
                         <div className="bg-white p-3">
-                            <div className="border border-black flex flex-col gap-2 p-16 bg-white items-center">
+                            <div className="border border-black flex flex-col gap-2 p-16 max-lg:p-8 bg-white items-center">
                                 <div className="flex gap-2 items-center">
-                                    <div className="lg:text-lg xl:text-xl 2xl:text-2xl">tarif</div>
-                                    <div className="underline font-bold lg:text-xl xl:text-2xl 2xl:text-3xl">ADULTE</div>
+                                <div className="xxxs:text-md xxs:text-lg lg:text-lg xl:text-xl 2xl:text-2xl">tarif</div>
+                                <div className="underline font-bold xxxs:text-xl xxs:text-2xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono">ADULTE</div>
                                 </div>
-                                <div className="lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-green-500">650€</div>
-                                <div className="lg:text-lg xl:text-xl 2xl:text-2xl">/an</div>
+                                <div className="xxxs:text-2xl xxs:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-green-500">650€</div>
+                                <div className="xxxs:text-lg xxs:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">/an</div>
                             </div>
                         </div>
                         <div className="bg-black flex flex-col gap-2 p-10 text-white items-start">
@@ -50,13 +50,13 @@ const CotisationPage = () => {
                     </div>
                     <div className="flex flex-col">
                         <div className="bg-white p-3">
-                            <div className="border border-black flex flex-col gap-2 p-16 bg-white items-center">
+                            <div className="border border-black flex flex-col gap-2 p-16 max-lg:p-8 bg-white items-center">
                                 <div className="flex gap-2 items-center">
-                                    <div className="lg:text-lg xl:text-xl 2xl:text-2xl">tarif</div>
-                                    <div className="underline font-bold lg:text-xl xl:text-2xl 2xl:text-3xl">ENFANT</div>
+                                <div className="xxxs:text-md xxs:text-lg lg:text-lg xl:text-xl 2xl:text-2xl">tarif</div>
+                                <div className="underline font-bold xxxs:text-xl xxs:text-2xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono">ENFANT</div>
                                 </div>
-                                <div className="lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-green-500">75€</div>
-                                <div className="lg:text-lg xl:text-xl 2xl:text-2xl">/mois</div>
+                                <div className="xxxs:text-2xl xxs:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-green-500">75€</div>
+                                <div className="xxxs:text-lg xxs:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">/mois</div>
                             </div>
                         </div>
                         <div className="bg-black flex flex-col gap-2 p-10 text-white items-start">
@@ -70,17 +70,17 @@ const CotisationPage = () => {
             <div className="flex justify-center w-full mt-10">
                     <div className="w-full max-w-screen-2xl flex items-center flex-col gap-5">
                         <div className="w-full h-px bg-black "></div> 
-                        <div className="lg:text-lg xl:text-xl 2xl:text-2xl font-mono font-family: ui-monospace">+ 50€ de frais d’assurance</div>
-                        <div className="lg:text-lg xl:text-xl 2xl:text-2xl font-mono font-family: ui-monospace">+ 50€ de frais de licence</div>
-                        <div className="lg:text-lg xl:text-xl 2xl:text-2xl font-mono font-family: ui-monospace">/ an</div>
+                        <div className="xxxs:text-md xxs:text-lg lg:text-xl xl:text-xl 2xl:text-2xl font-mono font-family: ui-monospace">+ 50€ de frais d’assurance</div>
+                        <div className="xxxs:text-md xxs:text-lg lg:text-xl xl:text-xl 2xl:text-2xl font-mono font-family: ui-monospace">+ 50€ de frais de licence</div>
+                        <div className="xxxs:text-lg xxs:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">/ an</div>
                         <div className="w-full h-px bg-black "></div> 
                     </div>
             </div>
             <div className="flex justify-center w-full mt-10">
                     <div className="w-full max-w-screen-2xl flex items-center flex-col gap-5 background border p-3">
-                        <div className="lg:text-lg xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">COURS PRIVÉ</div>
-                        <div className="lg:text-3xl xl:text-4xl 2xl:text-5xl font-mono font-family: ui-monospace font-bold text-green-500">50€</div>
-                        <div className="lg:text-xl xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">/heure</div>
+                        <div className="xxxs:text-xl xxs:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl font-mono font-family: ui-monospace underline">COURS PRIVÉ</div>
+                        <div className="xxxs:text-2xl xxs:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-green-500">50€</div>
+                        <div className="xxxs:text-lg xxs:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl font-mono font-family: ui-monospace">/heure</div>
                     </div>
             </div>
     </div>
