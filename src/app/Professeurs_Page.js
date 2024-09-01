917a55d40ced4f096bd0684af83d7cab99725280
ks <ksainte19@gmail.com>
2024-09-01 14:36:43 +0200

    ok

--- src/app/Professeurs_Page.js
+++ src/app/Professeurs_Page.js
@@ -28,7 +28,7 @@ const ProfesseursPage = () => {
                     <div className="flex flex-col max-md:px-24 max-sm:px-10 max-md:object-contain">
                         <Image src={images.d1}  className="md:h-[20rem] lg:h-[24rem] xl:h-[30rem] 2xl:h-[35rem]" style={DisciplineStyle} alt="gallery_image"/>
                         <div className=" bg-purple-700 flex flex-col gap-2 p-10 max-lg:p-8 text-white items-center">
-                            <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace">YULI NAGATA</div>
+                            <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">YULI NAGATA</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur karate, K1, MMA</div>
                             <div className="xxxs:text-sm xxs:text-base xs:text-lg sm:text-xl md:text-lg lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace">
                             Enim neque volutpat ac tincidunt vitae semper. Id diam vel quam elementum pulvinar etiam non.
@@ -39,7 +39,7 @@ const ProfesseursPage = () => {
                     <div className="flex flex-col max-md:px-24 max-sm:px-10 max-md:object-contain">
                         <Image src={images.d1} className="md:h-[20rem] lg:h-[24rem] xl:h-[30rem] 2xl:h-[35rem]" style={DisciplineStyle} alt="gallery_image"/>
                         <div className="bg-purple-700 flex flex-col gap-2 p-10 max-lg:p-8 text-white items-center">
-                            <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace">ARTHUR BUYSSENS</div>
+                            <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">ARTHUR BUYSSENS</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur karate, K1 </div>
                             <div className="xxxs:text-sm xxs:text-base xs:text-lg sm:text-xl md:text-lg lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace">
                             Enim neque volutpat ac tincidunt vitae semper. Id diam vel quam elementum pulvinar etiam non.
@@ -50,7 +50,7 @@ const ProfesseursPage = () => {
                     <div className="flex flex-col max-md:px-24 max-sm:px-10">
                         <Image src={images.d1}  className="md:h-[20rem] lg:h-[24rem] xl:h-[30rem] 2xl:h-[35rem]" style={DisciplineStyle} alt="gallery_image"/>   
                         <div className="bg-purple-700 flex flex-col gap-2 p-10 max-lg:p-8 text-white items-center">
-                            <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace">MARWAN OIHDI</div>
+                            <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">MARWAN OIHDI</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">assistant professeur kids</div>
                             <div className="xxxs:text-sm xxs:text-base xs:text-lg sm:text-xl md:text-lg lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace">
                             Enim neque volutpat ac tincidunt vitae semper. Id diam vel quam elementum pulvinar etiam non.
