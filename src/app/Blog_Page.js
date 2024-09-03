3cb1e9366c818db494f0c0d321b3c11faca55ab8
ks <ksainte19@gmail.com>
2024-09-03 10:33:20 +0200

    ok

--- src/app/Blog_Page.js
+++ src/app/Blog_Page.js
@@ -30,20 +30,20 @@ function Blog() {
         <div className="mx-10 hidden max-md:flex col-span-12 background bg-white justify-center py-1 lg:text-lg xl:text-xl 2xl:text-2xl font-mono font-family:ui-monospace border-4 border-black text-center">
           AND MORE TO COME
         </div>
-        <div className="flex justify-center items-center col-span-12 max-lg:mt-5 overflow-x-hidden">
+        <div className="hidden  max-xs:flex 2xl:flex justify-center items-center col-span-12  overflow-x-hidden">
           <div className="xxxs:max-w-screen-xs xxs:max-w-screen-xs xs:max-w-screen-xs sm:max-w-screen-sm md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl">
-          <div className=" flex-grow h-px bg-black "></div>
-            <div className="my-5 text-black flex justify-center font-mono	font-family: ui-monospace lg:text-xl xl:text-2xl 2xl:text-3xl italic">Instagram </div>
-            <div className=" flex-grow h-px bg-black "></div>
-            <div className="max-xs:mx-14 max-xxs:ml-20">
+          {/* <div className=" flex-grow h-px bg-black "></div> */}
+            <div className=" mb-5 text-black flex justify-center font-mono	font-family: ui-monospace lg:text-xl max-xs:text-xl xl:text-2xl 2xl:text-3xl italic underline">Instagram </div>
+            {/* <div className=" flex-grow h-px bg-purple-500 "></div> */}
+            <div className="max-xs:mx-14 max-xxs:ml-20 2xl:mt-10">
             <Gallery 
               images={[images.homepage2, images.homepage1, images.blog3, images.blog2, images.blog1, images.homepage3, images.homepage3]}
               flexDirection="row"
               display="none"
-              heightClass=" xxxs:h-[11rem] xxs:h-[13rem] xs:h-[16rem] sm:h-[18rem] md:h-[22rem] lg:h-[25rem] xl:h-[30rem] 2xl:h-[30rem]" 
-              minWidthClass=" xxxs:min-w-[16rem] xxs:min-w-[18rem] xs:min-w-[23rem] sm:min-w-[25rem] md:min-w-[30rem] lg:min-w-[35rem] xl:min-w-[35rem] 2xl:min-w-[35rem]"
+              heightClass=" xxxs:h-[11rem] xxs:h-[13rem] xs:h-[16rem] sm:h-[18rem] md:h-[20rem] lg:h-[25rem] xl:h-[30rem] 2xl:h-[30rem]" 
+              minWidthClass=" xxxs:min-w-[16rem] xxs:min-w-[18rem] xs:min-w-[21rem] sm:min-w-[23rem] md:min-w-[25rem] lg:min-w-[35rem] xl:min-w-[35rem] 2xl:min-w-[35rem]"
               marginLeft="2rem"
-              marginTop="2rem"
+              // marginTop="2rem"
             />
             </div>
           </div>
