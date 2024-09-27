f6019d5ef0df6a28d4d5616d031501f29ba0b768
ks <ksainte19@gmail.com>
2024-09-27 19:25:02 +0200

    ok

--- src/app/Professeurs_Page.js
+++ src/app/Professeurs_Page.js
@@ -25,34 +25,34 @@ const ProfesseursPage = () => {
     
             <div className="flex justify-center w-full mt-10 px-2">
                 <div className="flex max-md:flex-col justify-evenly items-center w-full xs:max-w-screen-xs sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl gap-4 xl:gap-10 ">
-                    <div className="flex flex-col max-md:w-3/4 w-1/3  h-2/3 xs:h-[50rem]  2xl:h-[55rem]">
-                        <Image src={images.Yuli3}  className="h-full" style={DisciplineStyle} alt="gallery_image"/>
+                    <div className="flex flex-col max-md:w-3/4 w-1/3 xs:h-[50rem]  2xl:h-[55rem]">
+                        <Image src={images.Yuli3}  className="max-xs:h-[25rem] h-full" style={DisciplineStyle} alt="gallery_image"/>
                         <div className=" bg-purple-700 flex flex-col gap-2 p-8 max-md:p-8 text-white items-center">
                             <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">YULI NAGATA</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur karate, K1, MMA</div>
-                            <div className="xxxs:text-sm xxs:text-base xs:text-base sm:text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace max-xs:text-center">
-                            3rd Dan Shidokan Karate, 2019 World Champion, fights in Karate-K1-MMA, passionate by Martial Arts, he teaches for 8 years and now struggles with learning Jiu-Jitsu .
+                            <div className="xxxs:text-sm xxs:text-base xs:text-base sm:text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace max-xs:text-left">
+                            3rd Dan Shidokan Karate, 2019 World Champion, fights in Karate-K1-MMA, passionate by Martial Arts, he teaches for 8 years and now struggles with learning Jiu-Jitsu.
                             </div>
                             <div className="w-full h-px bg-white mt-3"></div> 
                         </div>
                     </div>
-                    <div className="flex flex-col max-md:w-3/4 w-1/3 h-2/3 xs:h-[50rem] 2xl:h-[55rem]">
-                        <Image src={images.Arthur} className="h-full" style={DisciplineStyle} alt="gallery_image"/>
+                    <div className="flex flex-col max-md:w-3/4 w-1/3 xs:h-[50rem] 2xl:h-[55rem]">
+                        <Image src={images.Arthur} className="max-xs:h-[25rem] h-full " style={DisciplineStyle} alt="gallery_image"/>
                         <div className="bg-purple-700 flex flex-col gap-2 p-8 max-lg:p-8 text-white items-center">
                             <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">ARTHUR BUYSSENS</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur karate, K1, MMA </div>
-                            <div className="xxxs:text-sm xxs:text-base xs:text-base sm:text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace max-xs:text-center">
+                            <div className="xxxs:text-sm xxs:text-base xs:text-base sm:text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace max-xs:text-left">
                             2nd Dan Shidokan Karate, competes in multiple tournaments all over the world, he now brings his expertise in diverse classes at Shidostudio.
                             </div>
                             <div className="w-full h-px bg-white mt-3"></div> 
                         </div>
                     </div>
-                    <div className="flex flex-col max-md:w-3/4 w-1/3 h-2/3 xs:h-[50rem] 2xl:h-[55rem]">
-                        <Image src={images.Tiago}  className="h-full" style={DisciplineStyle} alt="gallery_image"/>   
+                    <div className="flex flex-col max-md:w-3/4 w-1/3  xs:h-[50rem] 2xl:h-[55rem]">
+                        <Image src={images.Tiago}  className="max-xs:h-[25rem] h-full" style={DisciplineStyle} alt="gallery_image"/>   
                         <div className="bg-purple-700 flex flex-col gap-2 p-8 max-lg:p-8 text-white items-center">
                             <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">TIAGO OLIVEIRA</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur karate, K1, MMA</div>
-                            <div className="xxxs:text-sm xxs:text-base xs:text-base sm:text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace max-xs:text-center">
+                            <div className="xxxs:text-sm xxs:text-base xs:text-base sm:text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg font-mono	font-family: ui-monospace max-xs:text-left">
                             2nd Dan Shidokan Karate, started young with Kung-Fu and masters now different disciplines, still competing, though he's heavyweight, he shows patience with beginners.
                             </div>
                             <div className="w-full h-px bg-white mt-3"></div> 
@@ -63,8 +63,8 @@ const ProfesseursPage = () => {
 
             <div className="flex justify-center w-full mt-10 px-2">
                 <div className="flex max-md:flex-col justify-evenly items-center w-full xs:max-w-screen-xs sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl gap-4 xl:gap-10">
-                    <div className="flex flex-col max-md:w-3/4 w-1/3  h-[45rem] xs:h-[50rem] 2xl:h-[55rem]">
-                        <Image src={images.Said}  className="h-full" style={DisciplineStyle} alt="gallery_image"/>
+                    <div className="flex flex-col max-md:w-3/4 w-1/3   xs:h-[50rem] 2xl:h-[55rem]">
+                        <Image src={images.Said}  className="max-xs:h-[25rem] h-full" style={DisciplineStyle} alt="gallery_image"/>
                         <div className=" bg-purple-700 flex flex-col gap-2 p-8 max-lg:p-8 text-white items-center">
                             <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">SAID CHAABI</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur grappling</div>
@@ -74,8 +74,8 @@ const ProfesseursPage = () => {
                             <div className="w-full h-px bg-white mt-3"></div> 
                         </div>
                     </div>
-                    <div className="flex flex-col max-md:w-3/4 w-1/3  h-[45rem] xs:h-[50rem] 2xl:h-[55rem]">
-                        <Image src={images.Mubarak} className="h-full" style={DisciplineStyle} alt="gallery_image"/>
+                    <div className="flex flex-col max-md:w-3/4 w-1/3   xs:h-[50rem] 2xl:h-[55rem]">
+                        <Image src={images.Mubarak} className="max-xs:h-[25rem] h-full" style={DisciplineStyle} alt="gallery_image"/>
                         <div className="bg-purple-700 flex flex-col gap-2 p-8 max-lg:p-8 text-white items-center">
                             <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">MOUBARAK BOULAICH</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur judo </div>
@@ -85,8 +85,8 @@ const ProfesseursPage = () => {
                             <div className="w-full h-px bg-white mt-3"></div> 
                         </div>
                     </div>
-                    <div className="flex flex-col max-md:w-3/4 w-1/3  h-[45rem] xs:h-[50rem] 2xl:h-[55rem]">
-                        <Image src={images.Saber}  className="h-full" style={DisciplineStyle} alt="gallery_image"/>   
+                    <div className="flex flex-col max-md:w-3/4 w-1/3   xs:h-[50rem] 2xl:h-[55rem]">
+                        <Image src={images.Saber}  className="max-xs:h-[25rem] h-full" style={DisciplineStyle} alt="gallery_image"/>   
                         <div className="bg-purple-700 flex flex-col gap-2 p-8 max-lg:p-8 text-white items-center">
                             <div className="xxxs:text-xl xxs:text-2xl xl:text-2xl 2xl:text-3xl font-bold font-mono	font-family: ui-monospace text-center">SABER EL MOUDNI</div>
                             <div className="xxxs:text-lg xxs:text-lg xs:text-xl sm:text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl font-mono	font-family: ui-monospace">professeur judo</div>
